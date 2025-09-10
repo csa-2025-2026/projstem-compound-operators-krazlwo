4b25e89a71fc89713cf1f81065d36ef46c88332a
krazlwo <12345esd.me@gmail.com>
2025-09-10 18:05:09 +0000

    3q446

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,16 @@
+public class Problem2
+    
+    public static void main(String[] args)
+    {
+        int x = 143;
+        int lastdig = (x % mod);
+        System.out.println(lastdig);
 
+        int y = (x / 10);
+        y = (y % 10);
+        System.out.println(y);
+
+        int g = (x / 100);
+        g = (g % 10);
+        System.out.println(g);
+    }
